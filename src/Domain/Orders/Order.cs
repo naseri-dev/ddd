9278264fc3b10135e9156533e9fc2e8201036833
naseri-dev <dev.naseri@gmail.com>
2023-04-13 18:12:18 +0430

    Domain Layer done

---
+++ src/Domain/Orders/Order.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Customers;
+using Domain.Products;
+
+namespace Domain.Orders;
+
+public class Order
+{
+	private readonly HashSet<LineItem> _lineItems = new();
+	public Guid Id { get; private set; }
+	public Guid CustomerId { get; private set; }
+
+	private Order() { }
+
+	public static Order Create(Customer customer)
+	{
+		var order = new Order()
+		{
+			Id = Guid.NewGuid(),
+			CustomerId = customer.Id
+		};
+		return order;
+	}
+	public void Add(Product product)
+	{
+		var lineItem = new LineItem(Guid.NewGuid(), Id, product.Id, product.Price);
+		_lineItems.Add(lineItem);
+	}
+}
---
+++ src/Domain/Products/Sku.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Products;
+
+public record Sku
+{
+	private const int DefaultValue = 15;
+	public string Value { get; init; }
+	private Sku(string value) => Value = value;
+
+	public static Sku? Create(string value)
+	{
+		if (string.IsNullOrEmpty(value)) 
+		{ return null; }
+		if (value.Length != DefaultValue) return null;
+
+		return new Sku(value);
+	}
+}
